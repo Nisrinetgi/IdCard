2cd9058429d0c9ad11edd67026c2d7c66bbb8a5e
Nisrinetgi <t.nisrine96@gmail.com>
2020-07-22 09:46:28 +0200

    Flutter app 1

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: IdCard(),
+  ));
+}
+
+class IdCard extends StatefulWidget {
+  @override
+  _IdCardState createState() => _IdCardState();
+}
+
+class _IdCardState extends State<IdCard> {
+  int level = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+
+        onPressed: () {
+          setState(() {
+            level +=1;
+          });
+        },
+
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/mario.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )
+            ),
+
+            SizedBox(height: 10.0),
+            Text(
+                'Mario',
+                style: TextStyle(
+                    color: Colors.red,
+                    letterSpacing: 2.0,
+                    fontSize:28.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(height: 30.0),
+            Text(
+                'CURRENT LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )
+            ),
+
+            SizedBox(height: 10.0),
+            Text(
+                '$level',
+                style: TextStyle(
+                    color: Colors.red,
+                    letterSpacing: 2.0,
+                    fontSize:28.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                Text(
+                  'mario@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+      ),
+
+    );
+  }
+}
